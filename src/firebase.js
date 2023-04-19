eda641f9293023544094a773ae75edb9092b6234
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-04-19 15:39:46 +0900

    a

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCpKbiJP0CPiD16MMpFxXY_9qnDFYzpTGE",
+  authDomain: "kyun-sns.firebaseapp.com",
+  projectId: "kyun-sns",
+  storageBucket: "kyun-sns.appspot.com",
+  messagingSenderId: "544499169554",
+  appId: "1:544499169554:web:4777e6f8d3ee218e6f5bcb",
+};
+
+const app = initializeApp(firebaseConfig);

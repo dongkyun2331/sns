496c8a9b399d2cbbd19007061e887de195cea0ae
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-04-19 15:37:58 +0900

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Home />
+      </div>
+    </BrowserRouter>
+  );
 }

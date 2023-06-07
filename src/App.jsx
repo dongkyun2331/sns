8065521b8cc7c1ae135648c483cd22f2e63f8466
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-06-07 17:10:29 +0900

    firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <Header />
-      </div>
-    </BrowserRouter>
-  );
+function App() {
+  return <div>App</div>;
 }
+
+export default App;
